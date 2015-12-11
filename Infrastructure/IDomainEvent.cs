32bcdc743edf69f838d8abdf6cc947ee8e902c3f
Lokf <wistrom.fredrik@gmail.com>
2015-12-11 12:28:01 +0100

    Added aggregate base and domain event.

---
+++ Infrastructure/IDomainEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace Lokf.Library.Infrastructure
+{
+    using System;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IDomainEvent
+    {
+        Guid AggregateId { get; }
+        Guid Id { get; }
+        int Version { get; }
+    }
+}
